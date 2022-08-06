46bb98e6e993e06ceec47c8a172f311c92f6f08d
Tyree Webber <webber.tyree@gmail.com>
2022-08-06 12:11:56 -0600

    remove firebase

--- app.js
+++ app.js
@@ -1,17 +1,13 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import Phaser from 'phaser'
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyDCwHfNZjWDlmHXNfOHRt2OZLgKfyeOUFw",
-  authDomain: "lhl-final-2b194.firebaseapp.com",
-  projectId: "lhl-final-2b194",
-  storageBucket: "lhl-final-2b194.appspot.com",
-  messagingSenderId: "174850367496",
-  appId: "1:174850367496:web:5f7f98433ad189ef4c31bd"
+let gameScene = new Phaser.Scene('Game');
+
+gameScene.init = function() {};
+
+gameScene.preload = function() {
+
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+gameScene.create = function() {
+  
+};
